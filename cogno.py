d06597d188a4df529fb8b97fd1e0d90f9451e152
jconnershelton <j.conner.shelton@gmail.com>
2021-07-03 09:42:28 -0400

    Add facial detection when name is said

--- cogno.py
+++ cogno.py
@@ -1,5 +1,24 @@
+from FacialRecognition import FacialIdentifier
+from speech import recog
+import FacialRecognition
+import cv2 as cv
+
 def run():
-    pass
+    rec = recog.SpeechRecognizer()
+    camera = cv.VideoCapture(0)
+    identifier = FacialRecognition.FacialIdentifier()
+
+    print('Done with setup.')
+
+    while True:
+        name = rec.listen()
+
+        if name:
+            ret, frame = camera.read()
+            cv.cvtColor(frame, cv.COLOR_BGR2RGB, dst=frame)
+            identifier.add_face(frame, name)
+
+            recog.notify_name_captured()
 
 if __name__ == '__main__':
     run()
