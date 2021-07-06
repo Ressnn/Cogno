d655e9051e747b8dce89c2a9028fa350b47886b3
Ressnn <42896924+Ressnn@users.noreply.github.com>
2021-07-06 09:02:03 -0400

    Restart

--- cogno.py
+++ cogno.py
@@ -1,28 +1,55 @@
 
-import Recognition
-import multiprocessing
+from Recognition import FacialIdentifier, SpeechRecognition
+from multiprocessing import Process, Pipe
+import logging
+import time
 
 class MainThreadWorker():
     def __init__(self):
-        pass
+        logging.info("Main Process Started")
+        self._kickoff_workers()
+
     def _kickoff_workers(self):
+        logging.info("Starting Child Processes")
+        F,S = self._generate_pipes()
+        self.FaceProcess = FacialRecognitionChildProcess(F)
+        self.SpeechProcess = SpeechRecognitionChildProcess(S)
+
+        self.FaceProcess.start()
+        self.SpeechProcess.start()
+
+    def _generate_pipes(self):
+        self.FaceProcessPipe, givetoF = Pipe()
+        self.SpeechProcessPipe, givetoS = Pipe()
+        return givetoF,givetoS
+
+    def main_loop():
         pass
 
-class FacialRecognitionChildProcess():
-    def __init__(self):
-        pass
+class FacialRecognitionChildProcess(Process):
+    def __init__(self,pipe):
+        super(Process, self).__init__()
+        self.FaceCog = FacialIdentifier()
+        self.mainpipe = pipe
+        logging.info("Facial Recognition Process Started")
+    def run(self):
+        while True:
+            print("HI")
 
-class SpeechRecognitionChildProcess():
-    def __init__(self):
-        pass
+class SpeechRecognitionChildProcess(Process):
+    def __init__(self,pipe):
+        super(Process, self).__init__()
+        self.SpeechCog = FacialIdentifier()
+        self.mainpipe = pipe
+        logging.info("Voice Recognition Process Started")
+    def run(self):
+        while True:
+            pass
 
 
-
-
-
-
-def run():
-    pass
 
 if __name__ == '__main__':
-    run()
+    M = MainThreadWorker()
+    time.sleep(1)
+    print(M.FaceProcess.is_alive())
+    print(M.SpeechProcess.is_alive())
