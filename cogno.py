5ed5326908451ace89a75ab9e6c9b2012e1d4689
Ressnn <42896924+Ressnn@users.noreply.github.com>
2021-07-12 10:40:39 -0400

    Changes

--- cogno.py
+++ cogno.py
@@ -92,6 +92,91 @@ class MainProcess():
         self.Face.add_face(frame,name)
         self.Face = FacialIdentifier(dbpath=self.f_path)
 
+class ServerProcess():
+    def __init__(self,display = False, facebase_path = "./Data/facebase", audiobase_path ="./Data/audiobase", flip_image = False):
+        """
+        Constructor for the MainProcess of cogno
+
+        Parameters
+        ----------
+        display : Boolean
+            Display Frames for Debugging?
+        facebase_path : String
+            path to facebase to match
+        audiobase_path : String
+            path to audiobase with pronunciations
+
+        Returns
+        -------
+        NoneType
+            None
+
+        """
+        self.flip = flip_image
+        self.Face = FacialIdentifier(dbpath=facebase_path)
+        self.AudioProcess = AudioBuffer(dbpath=audiobase_path)
+        self.display = display
+        self.f_path = facebase_path
+        self.a_path = audiobase_path
+        self.vid = cv2.VideoCapture(0)
+
+    def identify(self):
+        """
+        Identifies ther person in the frame and repeats the name back in the user's ear
+
+        Returns
+        -------
+        Boolean
+            True if face was discovered False if otherwise
+
+        """
+
+        ret, frame = self.vid.read()
+        if self.flip == True:
+            frame = cv2.flip(frame,0)
+
+        if self.display == True:
+            cv2.imshow('frame', frame)
+        try:
+            id = self.Face.get_person(frame,prob_threshold=1)
+            print(id)
+        except:
+            print("No Face in Frame")
+            return False
+
+        if id == -1:
+            return False
+
+        sounds = os.listdir(os.path.join(self.a_path,str(id)))
+        playsound(os.path.join(os.path.join(self.a_path,str(id)),sounds[0]))
+        return True
+
+    def add_person(self,name = str(uuid.uuid4())):
+        """
+        Adds a person to the audio and facebase
+
+        Parameters
+        ----------
+        name : String
+            The name to store a person by in the databases
+
+        Returns
+        -------
+        NoneType
+            None
+
+        """
+        self.AudioProcess.save(name)
+        ret, frame = self.vid.read()
+
+        if self.flip == True:
+            frame = cv2.flip(frame,0)
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.Face.add_face(frame,name)
+        self.Face = FacialIdentifier(dbpath=self.f_path)
+
+
 # Global variables used specifically for GPIO callback
 GPIO_action = None
 last_press = 0
---
+++ cognoserver.py
@@ -0,0 +1,77 @@
+import socket
+import sys
+import cv2
+import pickle
+import numpy as np
+import struct ## new
+import zlib
+
+HOST=''
+PORT=8485
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print('Socket created')
+
+s.bind((HOST,PORT))
+print('Socket bind complete')
+s.listen(10)
+print('Socket now listening')
+
+conn, addr=s.accept()
+
+def read_image():
+
+
+data = b""
+payload_size = struct.calcsize(">L")
+print("payload_size: {}".format(payload_size))
+
+while True:
+    # 1 for identification, 2 for addition
+    instruction = int.from_bytes(data.recv(4), 'little')
+
+    if instruction == 1:
+        size = int.from_bytes(data.recv(4), 'little')
+        img = b''
+
+        for _ in range(size // 4096):
+            img += conn.recv(4096)
+
+        img += conn.recv(size % 4096)
+        img = np.frombuffer(img, dtype=np.uint8)
+
+        
+    elif instruction == 2:
+        size = int.from_bytes(data.recv(4), 'little')
+        img = b''
+
+        while _ in range(size // 4096):
+            img += conn.recv(4096)
+
+        img += conn.recv(size % 4096)
+        img = np.frombuffer(img, dtype=np.uint8)
+
+
+    else:
+
+
+    while len(data) < payload_size:
+        print("Recv: {}".format(len(data)))
+        data += conn.recv(4096)
+
+    print("Done Recv: {}".format(len(data)))
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack(">L", packed_msg_size)[0]
+    print("msg_size: {}".format(msg_size))
+    while len(data) < msg_size:
+        data += conn.recv(4096)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+
+
+    frame = np.frombuffer(frame_data, dtype=np.uint8)
+    # frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    cv2.imshow('ImageWindow',frame)
+    cv2.waitKey(1)
